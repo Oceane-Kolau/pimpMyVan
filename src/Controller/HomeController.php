f28c1bdb6bebeabb9930462818f7cff7028fa2dd
Oceane-Kolau <Oceane-Kolau@users.noreply.github.com>
2021-02-04 00:09:23 +0100

    setup home page and filters

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Data\SearchArtisansData;
+use App\Form\SearchArtisansType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,8 +17,30 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/amenageurs-van", name="home_allArtisans")
+     */
+    public function allArtisans(UserRepository $userRepository, Request $request): Response 
+    {
+        $search = new SearchArtisansData();
+        $searchForm = $this->createForm(SearchArtisansType::class, $search);
+        $searchForm->handleRequest($request);
+        $allArtisans = $userRepository->searchArtisans($search);
+
+        return $this->render('home/allArtisans.html.twig', [
+            'artisans' => $allArtisans,
+            'searchForm' => $searchForm->createView()
         ]);
+    }
+
+    /**
+     * @Route("/amenageurs/{slug}", name="home_artisan_show")
+     */
+    public function showArtisan(): Response
+    {
+        return $this->render('home/show_artisan.html.twig');
     }
 }
